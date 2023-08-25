1c4cead5f9ff2be4ea6169a8f04d9e22e05fb031
Adhika Adhiwijna <adiwijna@gmail.com>
2023-08-25 14:35:22 +0700

    change header animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-between gap-y-6 py-8 lg:flex-row">
           <Link href="/">
-            <div className="relative top-0 text-3xl font-bold transition-all duration-300 hover:-top-1">
+            <div className="hover: relative cursor-pointer text-3xl transition-all duration-300 after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-8 after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:bg-accent">
               Adhika <span className="font-light">Adhiwijna</span>
               <span className="text-accent">.</span>
             </div>
